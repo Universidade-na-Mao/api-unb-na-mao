83fc60404cf8411ab4d934dca1d964c47a305f5a
cast-soliveira <990905>
2023-12-14 17:43:11 -0300

    bugfix, remove intervalo webscrapping

--- src/routes/scrap.js
+++ src/routes/scrap.js
@@ -10,13 +10,5 @@ router
     .route('/scrap/cardapio')
     .get((req, res) => scrappingController.scrapePDFLinks(req, res))
 
-const axios = require('axios');
-
-// Exemplo de requisição GET
-setInterval( async () => {
-    await axios.get('http://localhost:0203/api/scrap/cardapio')
-    .then(response => console.log(response))
-    .catch(error => console.error('Ocorreu um erro:', error));
-}, 604800000)
 
 module.exports = router
