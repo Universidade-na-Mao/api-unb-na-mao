2f9b8889a1fd65aee96c14b8141d35080a79cad1
BrenoLUCO <202017343@aluno.unb.br>
2024-01-30 16:20:40 -0300

    back disciplinas

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const usuariosRouter = require('./usuarios');
 const scrapsRouter = require('./scrap');
+const faltasRouter = require('./faltas');
+const avaliacaoRouter = require('./avaliacao');
+
 
 router.use('/', usuariosRouter);
 router.use('/', scrapsRouter);
+router.use('/', faltasRouter );
+router.use('/', avaliacaoRouter);
 
 module.exports = router;
